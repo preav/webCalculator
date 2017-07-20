5fda189b28a7d3acaa263110059e70640bc927c3
preav <preetiyadav.kit@gmail.com>
2017-07-20 22:42:00 +0530

    Responsive to be added

--- Script.js
+++ Script.js
@@ -1,6 +1,12 @@
 txtInput=document.getElementById("txtInput");
 txtResult=document.getElementById("txtResult");
-
+document.getElementById("btnPlus").addEventListener("click", operation, false);
+document.getElementById("btnMinus").addEventListener("click", operation, false);
+document.getElementById("calc").addEventListener("click", calculate, false);
+document.getElementById("clearAll").addEventListener("click", onClear, false);
+document.getElementById("clearLast").addEventListener("click", onClearLast, false);
+document.getElementById("btnMul").addEventListener("click", operation, false);
+document.getElementById("btnDiv").addEventListener("click", operation, false);
 for (var i=0;i<10;i++){
 document.getElementById("btn"+i).addEventListener("click", btnClick, false);
 }
@@ -9,23 +15,39 @@ function btnClick(){
 	txtInput.value=txtInput.value=="0"?this.innerText:txtInput.value+this.innerText;
 }
 
-
-document.getElementById("btnPlus").addEventListener("click", plus, false);
-document.getElementById("btnMinus").addEventListener("click", plus, false)
-function plus(){
+function operation(){
 	txtInput.value=txtInput.value+this.innerText;
 }
 
-document.getElementById("calc").addEventListener("click", calculate, false);
 
 function calculate(){
-	var index=txtInput.value.includes("+")?txtInput.value.lastIndexOf("+"):txtInput.value.lastIndexOf("-");
-	txtResult.value=Number(txtInput.value.substring(0, index))+Number(txtInput.value.substring(index));
+	var index=0;
+	// txtInput.value.includes("+")?txtInput.value.lastIndexOf("+"):txtInput.value.lastIndexOf("-");
+	if(txtInput.value.includes("+")){
+		index=txtInput.value.lastIndexOf("+");
+		txtInput.value=Number(txtInput.value.substring(0, index))+Number(txtInput.value.substring(index+1));
+	}
+	else if(txtInput.value.includes("-")){
+		index=txtInput.value.lastIndexOf("-");
+		txtInput.value=Number(txtInput.value.substring(0, index))-Number(txtInput.value.substring(index+1));
+	}
+	else if(txtInput.value.includes("x")){
+		index=txtInput.value.lastIndexOf("x");
+		txtInput.value=Number(txtInput.value.substring(0, index))*Number(txtInput.value.substring(index+1));
+	}
+	else {
+		index=txtInput.value.lastIndexOf("/");
+		txtInput.value=Number(txtInput.value.substring(0, index))/Number(txtInput.value.substring(index+1));
+	}
+	// txtInput.value=Number(txtInput.value.substring(0, index))+Number(txtInput.value.substring(index));
 }
-
-document.getElementById("clear").addEventListener("click", onClear, false);
 
 function onClear(){
 	txtInput.value=null;
+	txtResult.value=null;
+}
+
+function onClearLast(){
+	txtInput.value=txtInput.value.substring(0,(txtInput.value.length)-1);
 	txtResult.value=null;
 }
