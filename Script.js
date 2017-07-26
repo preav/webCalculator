dd067711d268aa3fa45c40a2fd39b365e929f90f
preav <preetiyadav.kit@gmail.com>
2017-07-26 19:14:43 +0530

    fixed table size redering differently in different browsers

--- Script.js
+++ Script.js
@@ -1,3 +1,4 @@
+(function(){
 txtInput=document.getElementById("txtInput");
 txtResult=document.getElementById("txtResult");
 document.getElementById("btnPlus").addEventListener("click", operation, false);
@@ -7,22 +8,14 @@ document.getElementById("clearAll").addEventListener("click", onClear, false);
 document.getElementById("clearLast").addEventListener("click", onClearLast, false);
 document.getElementById("btnMul").addEventListener("click", operation, false);
 document.getElementById("btnDiv").addEventListener("click", operation, false);
+document.getElementById("dec").addEventListener("click", addDec, false);
 for (var i=0;i<10;i++){
 document.getElementById("btn"+i).addEventListener("click", btnClick, false);
 }
-
-function btnClick(){
-	txtInput.value=txtInput.value=="0"?this.innerText:txtInput.value+this.innerText;
-}
-
-function operation(){
-	txtInput.value=txtInput.value+this.innerText;
-}
-
+}());
 
 function calculate(){
 	var index=0;
-	// txtInput.value.includes("+")?txtInput.value.lastIndexOf("+"):txtInput.value.lastIndexOf("-");
 	if(txtInput.value.includes("+")){
 		index=txtInput.value.lastIndexOf("+");
 		txtInput.value=Number(txtInput.value.substring(0, index))+Number(txtInput.value.substring(index+1));
@@ -35,11 +28,22 @@ function calculate(){
 		index=txtInput.value.lastIndexOf("x");
 		txtInput.value=Number(txtInput.value.substring(0, index))*Number(txtInput.value.substring(index+1));
 	}
+
 	else {
 		index=txtInput.value.lastIndexOf("/");
 		txtInput.value=Number(txtInput.value.substring(0, index))/Number(txtInput.value.substring(index+1));
 	}
-	// txtInput.value=Number(txtInput.value.substring(0, index))+Number(txtInput.value.substring(index));
+}
+
+function btnClick(){
+	txtInput.value=txtInput.value=="0"?this.innerText:txtInput.value+this.innerText;
+}
+
+function operation(){
+	txtInput.value=txtInput.value+this.innerText;
+}
+function addDec(){
+	txtInput.value=txtInput.value+".";
 }
 
 function onClear(){
